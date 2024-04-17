ad12f9d716a32a3c4d45ebe49e9e50b54f0347a5
Mateus <mateus.frauzino@gmail.com>
2024-04-17 15:01:16 -0300

    change 'add user' button to nav-item

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { getUsers } from '@/lib/db';
 import { UsersTable } from './users-table';
 import { Search } from './search';
-import { Button } from '@/components/ui/button';
+import { NavItem } from './nav-item';
 
 export default async function IndexPage({
   searchParams
@@ -16,9 +16,9 @@ export default async function IndexPage({
     <main className="flex flex-1 flex-col p-4 md:p-6">
       <div className="flex justify-between items-center mb-8">
         <h1 className="font-semibold text-lg md:text-2xl">Users</h1>
-        <Button size="lg" className="text-lg">
-          New user
-        </Button>
+        <nav className="text-lg h-11 rounded-md px-8 font-semibold border border-input bg-background hover:bg-accent hover:text-accent-foreground">
+          <NavItem href="/addUser">New user</NavItem>
+        </nav>
       </div>
       <div className="w-full mb-4">
         <Search value={searchParams.q} />
