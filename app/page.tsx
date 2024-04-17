9c2f7a133cc29217aa6ff5260f404812105882c9
Mateus <mateus.frauzino@gmail.com>
2024-04-17 13:05:38 -0300

    add 'new user' button

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { getUsers } from '@/lib/db';
 import { UsersTable } from './users-table';
 import { Search } from './search';
+import { Button } from '@/components/ui/button';
 
 export default async function IndexPage({
   searchParams
@@ -13,8 +14,11 @@ export default async function IndexPage({
 
   return (
     <main className="flex flex-1 flex-col p-4 md:p-6">
-      <div className="flex items-center mb-8">
+      <div className="flex justify-between items-center mb-8">
         <h1 className="font-semibold text-lg md:text-2xl">Users</h1>
+        <Button size="lg" className="text-lg">
+          New user
+        </Button>
       </div>
       <div className="w-full mb-4">
         <Search value={searchParams.q} />
