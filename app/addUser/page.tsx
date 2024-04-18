8a217a579a80b3825be9d235f5440378e3242d2d
Mateus <mateus.frauzino@gmail.com>
2024-04-18 14:04:21 -0300

    refactor 'AddUserPage

--- app/addUser/page.tsx
+++ app/addUser/page.tsx
@@ -1,3 +1,7 @@
+'use client';
+
+import { NewUserForm } from '@/components/ui/form';
+
 export default async function AddUserPage() {
   return (
     <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-6">
@@ -5,33 +9,7 @@ export default async function AddUserPage() {
         <h1 className="font-semibold text-lg md:text-2xl">Add User</h1>
       </div>
       <div>
-        <form id="new-user-form">
-          <div className="flex flex-col size-80 space-y-2">
-            <label htmlFor="name">Name</label>
-            <input
-              className="border border-black rounded-md"
-              type="text"
-              id="name"
-            ></input>
-            <label htmlFor="email">Email</label>
-            <input
-              className="border border-black rounded-md"
-              type="text"
-              id="email"
-            ></input>
-            <label htmlFor="username">Username</label>
-            <input
-              className="border border-black rounded-md"
-              type="text"
-              id="username"
-            ></input>
-            <input
-              className="border border-black rounded-lg w-20"
-              type="submit"
-              value="Add"
-            ></input>
-          </div>
-        </form>
+        <NewUserForm></NewUserForm>
       </div>
     </main>
   );
