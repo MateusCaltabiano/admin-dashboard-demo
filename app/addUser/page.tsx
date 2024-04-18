383c576421cbf3280751ae5f80b5a2e1dea1f9f3
Mateus <mateus.frauzino@gmail.com>
2024-04-18 09:38:17 -0300

    adding new user form

--- app/addUser/page.tsx
+++ app/addUser/page.tsx
@@ -4,6 +4,38 @@ export default async function AddUserPage() {
       <div className="flex items-center">
         <h1 className="font-semibold text-lg md:text-2xl">Add User</h1>
       </div>
+      <div>
+        <form>
+          <div className="flex flex-col size-80 space-y-2">
+            <input
+              className="border border-black rounded-md text-slate-400"
+              value="Name"
+              type="text"
+              id="name"
+              name="name"
+            ></input>
+            <input
+              className="border border-black rounded-md text-slate-400"
+              value="Email"
+              type="text"
+              id="email"
+              name="email"
+            ></input>
+            <input
+              className="border border-black rounded-md text-slate-400"
+              value="Username"
+              type="text"
+              id="username"
+              name="username"
+            ></input>
+            <input
+              className="border border-black rounded-lg w-20 self-auto"
+              type="submit"
+              value="Create"
+            ></input>
+          </div>
+        </form>
+      </div>
     </main>
   );
 }
