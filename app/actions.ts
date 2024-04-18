b4284dec228a81e02dc8b4d3cd15cd191c79603a
Mateus <mateus.frauzino@gmail.com>
2024-04-18 14:05:26 -0300

    update 'actions' with 'NewUser' function

--- app/actions.ts
+++ app/actions.ts
@@ -1,9 +1,34 @@
 'use server';
 
 import { deleteUserById } from '@/lib/db';
+import { addNewUser } from '@/lib/db';
 import { revalidatePath } from 'next/cache';
+import { z } from 'zod';
 
 export async function deleteUser(userId: number) {
   await deleteUserById(userId);
   revalidatePath('/');
+}
+
+export async function NewUser(prevState: any, formData: FormData) {
+  const schema = z.object({
+    name: z.string(),
+    email: z.string(),
+    username: z.string()
+  });
+
+  const data = schema.parse({
+    name: formData.get('name'),
+    email: formData.get('email'),
+    username: formData.get('username')
+  });
+
+  try {
+    await addNewUser(data.name, data.email, data.username);
+
+    revalidatePath('/');
+    return { message: `Added user ${data.name}` };
+  } catch (e) {
+    return { message: `Failed to add new user` };
+  }
 }
